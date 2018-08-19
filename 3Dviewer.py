ff9978ac6e381f5e61d4ae0519c891203b7dfe6a
Calciferzh <calciferzh@outlook.com>
2018-08-18 20:47:45 -0400

    add comments

--- 3Dviewer.py
+++ 3Dviewer.py
@@ -45,15 +45,25 @@ class SkeletonViewer:
 
     pygame.init()
     self.screen_size = (1024, 768)
-    self.screen = pygame.display.set_mode(self.screen_size, pygame.DOUBLEBUF | pygame.OPENGL)
-    pygame.display.set_caption('AMC Parser - frame %d / %d' % (self.frame, len(self.motions)))
+    self.screen = pygame.display.set_mode(
+      self.screen_size, pygame.DOUBLEBUF | pygame.OPENGL
+    )
+    pygame.display.set_caption(
+      'AMC Parser - frame %d / %d' % (self.frame, len(self.motions))
+    )
     self.clock = pygame.time.Clock()
 
     glClearColor(0, 0, 0, 0)
     glShadeModel(GL_SMOOTH)
-    glMaterialfv(GL_FRONT, GL_SPECULAR, np.array([1, 1, 1, 1], dtype=np.float32))
-    glMaterialfv(GL_FRONT, GL_SHININESS, np.array([100.0], dtype=np.float32))
-    glMaterialfv(GL_FRONT, GL_AMBIENT, np.array([0.7, 0.7, 0.7, 0.7], dtype=np.float32))
+    glMaterialfv(
+      GL_FRONT, GL_SPECULAR, np.array([1, 1, 1, 1], dtype=np.float32)
+    )
+    glMaterialfv(
+      GL_FRONT, GL_SHININESS, np.array([100.0], dtype=np.float32)
+    )
+    glMaterialfv(
+      GL_FRONT, GL_AMBIENT, np.array([0.7, 0.7, 0.7, 0.7], dtype=np.float32)
+    )
     glEnable(GL_POINT_SMOOTH)
 
     glLightfv(GL_LIGHT0, GL_POSITION, np.array([1, 1, 1, 0], dtype=np.float32))
@@ -208,18 +218,36 @@ class SkeletonViewer:
 
 class MeshViewer:
   def __init__(self, imitator, motions):
+    """
+    Display 3D SMPL model mesh using `pygame`.
+
+    Prameter
+    --------
+    imitator: Instance of `imitator.Imitator`, used to transfer asf motions to
+    SMPL model.
+
+    motions: A list of motions returned from 'reader.parse_amc'. Each item
+    should be a dict whose keys are asf joint names and values are rotation
+    degrees parsed from amc file.
+
+    """
     self.imitator = imitator
     self.motions = motions
     self.num_verts = 6890
     self.num_faces = 13776
     self.num_frames = len(self.motions)
+    # whether is dragging and the kind
     self.rorate_dragging = False
     self.translate_dragging = False
     self.old_x = 0
     self.old_y = 0
 
     storage = 9
-    vert_cnt = {i: 0 for i in range(self.num_verts)}
+    # how many faces each vertex belongs to
+    vert_cnt = np.zeros([self.num_verts], dtype=np.int)
+    # the faces each vertex belongs to
+    # this is used to compute vertex normal later
+    # which is the average of faces' normal it belongs to
     self.vftable = np.zeros((self.num_verts, storage), dtype=np.int32)
     for idx in range(self.num_faces):
       v1, v2, v3 = self.imitator.smpl.faces[idx]
@@ -229,23 +257,32 @@ class MeshViewer:
       vert_cnt[v1] += 1
       vert_cnt[v2] += 1
       vert_cnt[v3] += 1
+    # we use a "virtual face" to fill "blanks"
+    # this face's normal will be set to (0,0,0)
     for i in range(self.num_verts):
       for j in range(vert_cnt[i], storage):
         self.vftable[i, j] = self.num_faces
 
     pygame.init()
     self.screen_size = (1024, 768)
-    self.screen = pygame.display.set_mode(self.screen_size, pygame.DOUBLEBUF | pygame.OPENGL)
+    self.screen = pygame.display.set_mode(
+      self.screen_size, pygame.DOUBLEBUF | pygame.OPENGL
+    )
     pygame.display.set_caption('Frame %d / %d' % (0, self.num_frames))
     self.clock = pygame.time.Clock()
 
     glClearColor(0.0, 0.0, 0.0, 0.0)
     glShadeModel(GL_SMOOTH)
-    glMaterialfv(GL_FRONT, GL_SPECULAR, np.array([1, 1, 1, 1], dtype=np.float32))
-    glMaterialfv(GL_FRONT, GL_SHININESS, np.array([100.0], dtype=np.float32))
-    glMaterialfv(GL_FRONT, GL_AMBIENT, np.array([0.7, 0.7, 0.7, 0.7], dtype=np.float32))
-
-    glLightfv(GL_LIGHT0, GL_POSITION, np.array([1, 1, 1, 0], dtype=np.float32))
+    glMaterialfv(
+      GL_FRONT, GL_SPECULAR, np.array([1, 1, 1, 1], dtype=np.float32)
+    )
+    glMaterialfv(
+      GL_FRONT, GL_SHININESS, np.array([100.0], dtype=np.float32)
+    )
+    glMaterialfv(
+      GL_FRONT, GL_AMBIENT, np.array([0.7, 0.7, 0.7, 0.7], dtype=np.float32)
+    )
+    glLightfv(GL_LIGHT0, GL_POSITION, np.array([0, 0, 1, 0], dtype=np.float32))
     glEnable(GL_LIGHT0)
     glEnable(GL_LIGHTING)
     glEnable(GL_DEPTH_TEST)
@@ -254,109 +291,116 @@ class MeshViewer:
     gluPerspective(45, (self.screen_size[0]/self.screen_size[1]), 0.1, 50.0)
 
   def run(self):
-      global_rx = 0
-      global_ry = 0
-      std_trans = np.array([0, 0.22, -3], dtype=np.float32)
-      global_trans = std_trans
-      speed_rx = np.pi / 90
-      speed_ry = np.pi / 90
-      speed_trans = 0.01
-      speed_zoom = 0.1
-      frame = 0
-      done = False
-      playing = False
-      while not done:
-        for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-            done = True
-          elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-              if playing:
-                playing = False
-              else:
-                playing = True
-            if event.key == pygame.K_RETURN:
-              global_trans = std_trans
-              global_rx = 0
-              global_ry = 0
-          elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-              self.rorate_dragging = True
+    global_rx = 0
+    global_ry = 0
+    # default mesh tranlsation to put the mesh in the center of the window
+    std_trans = np.array([0, 0.22, -3], dtype=np.float32)
+    global_trans = std_trans
+    speed_rx = np.pi / 90
+    speed_ry = np.pi / 90
+    speed_trans = 0.01
+    speed_zoom = 0.1
+    frame = 0
+    done = False
+    playing = False
+    while not done:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          done = True
+        elif event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_SPACE: # pause / play
+            if playing:
+              playing = False
             else:
-              self.translate_dragging = True
-            self.old_x, self.old_y = event.pos
-          elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-              self.rorate_dragging = False
-            else:
-              self.translate_dragging = False
-          elif event.type == pygame.MOUSEMOTION:
-            if self.translate_dragging:
-              # haven't figure out best way to implement this
-              pass
-            elif self.rorate_dragging:
-              new_x, new_y = event.pos
-              global_ry -= (new_x - self.old_x) / self.screen_size[0] * np.pi
-              global_rx -= (new_y - self.old_y) / self.screen_size[1] * np.pi
-              self.old_x, self.old_y = new_x, new_y
-          pressed = pygame.key.get_pressed()
-          if pressed[pygame.K_DOWN]:
-            global_rx -= speed_rx
-          if pressed[pygame.K_UP]:
-            global_rx += speed_rx
-          if pressed[pygame.K_LEFT]:
-            global_ry += speed_ry
-          if pressed[pygame.K_RIGHT]:
-            global_ry -= speed_ry
-          if pressed[pygame.K_a]:
-            global_trans[0] -= speed_trans
-          if pressed[pygame.K_d]:
-            global_trans[0] += speed_trans
-          if pressed[pygame.K_w]:
-            global_trans[1] += speed_trans
-          if pressed[pygame.K_s]:
-            global_trans[1] -= speed_trans
-          if pressed[pygame.K_q]:
-            global_trans[2] += speed_zoom
-          if pressed[pygame.K_e]:
-            global_trans[2] -= speed_zoom
+              playing = True
+          if event.key == pygame.K_RETURN: # reset camera
+            global_trans = std_trans
+            global_rx = 0
+            global_ry = 0
+        elif event.type == pygame.MOUSEBUTTONDOWN: # dragging
+          if event.button == 1:
+            self.rorate_dragging = True
+          else:
+            self.translate_dragging = True
+          self.old_x, self.old_y = event.pos
+        elif event.type == pygame.MOUSEBUTTONUP:
+          if event.button == 1:
+            self.rorate_dragging = False
+          else:
+            self.translate_dragging = False
+        elif event.type == pygame.MOUSEMOTION:
+          if self.translate_dragging:
+            # haven't figure out best way to implement this
+            pass
+          elif self.rorate_dragging:
+            new_x, new_y = event.pos
+            global_ry -= (new_x - self.old_x) / self.screen_size[0] * np.pi
+            global_rx -= (new_y - self.old_y) / self.screen_size[1] * np.pi
+            self.old_x, self.old_y = new_x, new_y
+        pressed = pygame.key.get_pressed()
+        # rotation
+        if pressed[pygame.K_DOWN]:
+          global_rx -= speed_rx
+        if pressed[pygame.K_UP]:
+          global_rx += speed_rx
+        if pressed[pygame.K_LEFT]:
+          global_ry += speed_ry
+        if pressed[pygame.K_RIGHT]:
+          global_ry -= speed_ry
+        # translation
+        if pressed[pygame.K_a]:
+          global_trans[0] -= speed_trans
+        if pressed[pygame.K_d]:
+          global_trans[0] += speed_trans
+        if pressed[pygame.K_w]:
+          global_trans[1] += speed_trans
+        if pressed[pygame.K_s]:
+          global_trans[1] -= speed_trans
+        # zoom
+        if pressed[pygame.K_q]:
+          global_trans[2] += speed_zoom
+        if pressed[pygame.K_e]:
+          global_trans[2] -= speed_zoom
 
-        pygame.display.set_caption('Frame %d / %d' % (frame, self.num_frames))
+      pygame.display.set_caption('Frame %d / %d' % (frame, self.num_frames))
 
-        motion = self.motions[frame]
+      motion = self.motions[frame]
 
-        if playing:
-          frame += 1
-        if frame >= self.num_frames:
-          frame = 0
+      if playing:
+        frame += 1
+      if frame >= self.num_frames:
+        frame = 0
 
-        grx = euler.euler2mat(global_rx, 0, 0)
-        gry = euler.euler2mat(0, global_ry, 0)
+      grx = euler.euler2mat(global_rx, 0, 0)
+      gry = euler.euler2mat(0, global_ry, 0)
 
-        self.imitator.imitate(motion)
-        verts = self.imitator.smpl.verts
-        verts = verts.dot(grx).dot(gry) + global_trans
-        faces_coor = verts[self.imitator.smpl.faces]
+      self.imitator.imitate(motion)
+      verts = self.imitator.smpl.verts
+      verts = verts.dot(grx).dot(gry) + global_trans
+      faces_coor = verts[self.imitator.smpl.faces]
 
-        face_normals = graphics_np.get_normal(faces_coor)
-        face_normals = face_normals / np.linalg.norm(face_normals, axis=1, keepdims=True)
-        face_normals = np.append(face_normals, np.zeros((1,3)), axis=0)
+      face_normals = graphics_np.get_normal(faces_coor)
+      face_normals = face_normals / \
+        np.linalg.norm(face_normals, axis=1, keepdims=True)
+      # a virtual face to boost vertex normal computation
+      face_normals = np.append(face_normals, np.zeros((1,3)), axis=0)
 
-        vert_normals = face_normals[self.vftable]
-        vert_normals = np.sum(vert_normals, axis=1)
-        vert_normals = vert_normals / np.linalg.norm(vert_normals, axis=1, keepdims=True)
+      vert_normals = face_normals[self.vftable]
+      vert_normals = np.sum(vert_normals, axis=1)
+      vert_normals = vert_normals / \
+        np.linalg.norm(vert_normals, axis=1, keepdims=True)
 
-        verts = verts.astype(np.float32)
-        vert_normals = vert_normals.astype(np.float32)
+      verts = verts.astype(np.float32)
+      vert_normals = vert_normals.astype(np.float32)
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glVertexPointerf(verts)
-        glNormalPointerf(vert_normals)
-        glDrawElementsui(GL_TRIANGLES, self.imitator.smpl.faces)
-        pygame.display.flip()
-        self.clock.tick(120)
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+      glVertexPointerf(verts)
+      glNormalPointerf(vert_normals)
+      glDrawElementsui(GL_TRIANGLES, self.imitator.smpl.faces)
+      pygame.display.flip()
+      self.clock.tick(120)
 
-      pygame.quit()
+    pygame.quit()
 
 
 def test_skeleton():
@@ -384,4 +428,4 @@ def test_mesh():
 
 if __name__ == '__main__':
   test_mesh()
-  # test_skeleton()
+  test_skeleton()
